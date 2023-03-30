8f3db53fabc68ab07d6ad2ddc574604e8fb32eca
Erwan Foxtail <84564131+Erwan228@users.noreply.github.com>
2023-03-30 11:16:51 +0200

    laget onloine

---
+++ CodeAlong/Class1.cs
@@ -0,0 +1,33 @@
+﻿namespace Codealong
+{
+    public class Question
+    {
+        public string QuestionText { get; private set; }
+        public string[] Options { get; private set; }
+        public char RightAnswer { get; private set; }
+        public Question(string questionText, string[] options, char rightAnswer)
+        {
+            QuestionText = questionText;
+            Options = options;
+            RightAnswer = rightAnswer;
+        }
+        public void RunQuiz()
+        {
+            Console.WriteLine(QuestionText);
+            for (int i = 0; i < Options.Length; i++)
+            {
+                var letter = (Char)('A' + i);
+                string? question = Options[i];
+                Console.WriteLine(question);
+            }
+            Console.Write("Skriv inn a, b eller c: ");
+            var answer = Console.ReadLine();
+            if (answer[0] == RightAnswer)
+            {
+
+            }
+        }
+
+    }
+}
+
