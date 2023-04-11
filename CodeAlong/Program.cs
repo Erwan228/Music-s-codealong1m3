1e590dd3700f58e6a49ce693ac19d92da2849dd8
Erwan Foxtail <84564131+Erwan228@users.noreply.github.com>
2023-04-11 13:16:13 +0200

    quiz?

--- CodeAlong/Program.cs
+++ CodeAlong/Program.cs
@@ -1,28 +1,11 @@
-﻿using Codealong;
-
-namespace CodeAlong
+﻿namespace CodeAlong
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            string[] optionsArray = { "En karakter fra en visuell skrekkroman", "En Arkitekt", "En guvernør i Elysium" };
-            var Question1 = new Question("Hvem er Samuel Ayers", optionsArray, 'a');
-            string[] optionsArray2 = { "Minearbeider", "Butikkmedarbeider", "Utforsker", "Sheriff", "Hore" };
-            var Question2 = new Question("Hva er Sams opprinnelige yrke?", optionsArray2, 'e');
-            string[] optionsArray3 = { "Han falt ned fra en klippe", "Han druknet i innsjøen", "Han ble drept av en tulpa" };
-            var Question3 = new Question("Hvordan døde Sydney?", optionsArray3, 'b');
-            string[] optionsArray4 = { "Drukna av seg selv", "Chase", "Flynn", "Jenna", "Tobias", "Carl", "Leo" };
-            var Question4 = new Question("Hvem drukna Sydney", optionsArray4, 'b');
-            int points = 0;
-            points += Question1.RunQuestion();
-            points += Question2.RunQuestion();
-            points += Question3.RunQuestion();
-            Console.ForegroundColor = ConsoleColor.DarkRed;
-            points += Question4.RunQuestion();
-            Console.WriteLine(points + " Poeng");
-            Console.ForegroundColor = ConsoleColor.DarkBlue;
-            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            var Quiz1 = new Quiz();
+            Quiz1.RunQuiz();
 
 
 
