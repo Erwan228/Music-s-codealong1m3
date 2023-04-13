9a1e2a3ee60574feb6aa5ee45d56e540ae5f6afd
Erwan Foxtail <84564131+Erwan228@users.noreply.github.com>
2023-04-13 10:13:52 +0200

    ferdig

--- CodeAlong/Quiz.cs
+++ CodeAlong/Quiz.cs
@@ -4,24 +4,37 @@ namespace CodeAlong
 {
     public class Quiz
     {
-        public int RunQuiz()
+        List<Question> questions = new List<Question>();
+        public Quiz()
         {
             string[] optionsArray = { "En karakter fra en visuell skrekkroman", "En Arkitekt", "En guvernør i Elysium" };
             var Question1 = new Question("Hvem er Samuel Ayers", optionsArray, 'a');
+            questions.Add(Question1);
             string[] optionsArray2 = { "Minearbeider", "Butikkmedarbeider", "Utforsker", "Sheriff", "Hore" };
             var Question2 = new Question("Hva er Sams opprinnelige yrke?", optionsArray2, 'e');
+            questions.Add(Question2);
             string[] optionsArray3 = { "Han falt ned fra en klippe", "Han druknet i innsjøen", "Han ble drept av en tulpa" };
             var Question3 = new Question("Hvordan døde Sydney?", optionsArray3, 'b');
+            questions.Add(Question3);
             string[] optionsArray4 = { "Drukna av seg selv", "Chase", "Flynn", "Jenna", "Tobias", "Carl", "Leo" };
             var Question4 = new Question("Hvem drukna Sydney", optionsArray4, 'b');
+            questions.Add(Question4);
+        }
+        public int RunQuiz()
+        {
             int points = 0;
-            points += Question1.RunQuestion();
-            points += Question2.RunQuestion();
-            points += Question3.RunQuestion();
-            Console.ForegroundColor = ConsoleColor.DarkRed;
-            points += Question4.RunQuestion();
+            foreach (var question in questions)
+            {
+                points += question.RunQuestion();
+            }
+            //points += Question1.RunQuestion();
+            //points += Question2.RunQuestion();
+            //points += Question3.RunQuestion();
+            //Console.ForegroundColor = ConsoleColor.DarkRed;
+            //points += Question4.RunQuestion();
             return points;
 
         }
+
     }
 }
